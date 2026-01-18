bab7cf8a6b3ba88d8e7a0ab28c3e62a8c2e5eca1
Matheus <skt.matheus@hotmail.com>
2026-01-18 14:25:32 -0300

    adding initial project setup: .gitignore, .gitattributes, application.properties, and BUILD_GUIDE.md files

---
+++ apps/services/java-resource-service/src/main/java/com/polystack/javaresourceservice/JavaResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polystack.javaresourceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResourceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaResourceServiceApplication.class, args);
+    }
+
+}
